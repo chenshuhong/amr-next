d83260c9fd1d449e389b2d4e755f0e0f12451b55
csh <baiye.csh@dtyunxi.com>
2019-06-24 19:55:06 +0800

    fix(修复首页跳转不了问题):

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from 'next/link'
-import './index.less'
+import './index.less' //fix https://github.com/zeit/next-plugins/issues/282
 
 export default () => <div className='example'>
   <Link href="/hello">
