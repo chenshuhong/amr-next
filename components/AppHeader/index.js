f0e7e41509b8f22439a2ce294c81087406690ca6
shuhong <1573391005@qq.com>
2019-07-01 04:01:34 +0800

    build(antd设置):

--- components/AppHeader/index.js
+++ components/AppHeader/index.js
@@ -3,13 +3,24 @@
  * @Date: 2019-06-28 11:28
  */
 import React from "react";
+import { Menu } from 'antd';
 import style from './index.less'
 
 export default class AppHeader extends React.Component{
   render(){
     return (
       <div>
-        我是头部
+        <div className={style.logo} />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
       </div>
     )
   }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
-const withLess = require('@zeit/next-less')
-module.exports = withLess({
+const antdLessLoader = require("next-antd-aza-less")
+module.exports = antdLessLoader({
   lessLoaderOptions: {
     javascriptEnabled: true,
   },
@@ -13,11 +13,30 @@ module.exports = withLess({
     // Perform customizations to webpack config
     // Important: return the modified config
 
-    console.log(config.module.rules,defaultLoaders)
     // 目录别名配置
-    Object.assign(config.resolve.alias,{
+    Object.assign(config.resolve.alias, {
       'components': path.join(__dirname, 'components'),
     })
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/
+      const origExternals = [...config.externals]
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback()
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback)
+          } else {
+            callback()
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ]
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      })
+    }
     return config
   },
 })
